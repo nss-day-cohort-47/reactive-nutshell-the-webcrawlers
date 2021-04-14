9e2c66c185c49c39b88198bc332b12d97128efe5
Sidney Crandall <sdncrandall5@gmail.com>
2021-04-14 11:51:54 -0500

    minor fixes, before moving to messages

---
+++ src/data/messageManager.js
@@ -0,0 +1,38 @@
+const remoteURL = "http://localhost:8088"
+
+// fetch calls used to grab data 
+export const getAllMessages = () => {
+    return fetch(`${remoteURL}/messages`)
+        .then(result => result.json())
+}
+
+export const deleteMessage = (id) => {
+    return fetch(`${remoteURL}/messages/${id}`, {
+        method: "DELETE"
+    }).then(result => result.json())
+}
+
+export const getMessageById = (id) => {
+    return fetch(`${remoteURL}/messages/${id}?_expand=user`)
+        .then(res => res.json())
+}
+
+export const addMessage = (newMessage) => {
+    return fetch(`${remoteURL}/messages`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newMessage)
+    }).then(response => response.json())
+}
+
+export const updateMessage = (editedMeassage) => {
+    return fetch(`${remoteURL}/messages/${editedMeassage.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(editedMessage)
+    }).then(data => data.json());
+}
