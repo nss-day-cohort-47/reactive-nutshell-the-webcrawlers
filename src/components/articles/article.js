72286095c204fc591f8dae01db680639aaba0bd7
Sidney Crandall <sdncrandall5@gmail.com>
2021-04-14 14:16:57 -0500

    started messages. Pushing up minor chnages from articles after last push

--- src/components/articles/article.js
+++ src/components/articles/article.js
@@ -18,4 +18,4 @@ export const ArticleCard = ({ article, handleDeleteArticle }) => {
         </div>
     </div>
     )
-}
+};
--- src/components/messages/message.js
+++ src/components/messages/message.js
@@ -1,2 +1,16 @@
+// Authored by: Sidney Crandall
+// Card used to return Dom representation of messages.
+
 import React from "react";
 
+// Export function to show the message
+export const MessageCard = () => {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <p className="card-message">{message.message}</p>
+                <p>{message.timestamp}</p>
+            </div>
+        </div>
+    )
+};
