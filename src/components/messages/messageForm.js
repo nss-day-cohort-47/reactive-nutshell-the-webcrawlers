22c29cf41c8fdc3b73184547a58cfe4b376f59e2
Sidney Crandall <sdncrandall5@gmail.com>
2021-04-18 20:25:37 -0500

    Working on private messaging

--- src/components/messages/messageForm.js
+++ src/components/messages/messageForm.js
@@ -7,18 +7,20 @@ import { getAllUsers } from '../../data/usersManager';
 import { addMessage } from '../../data/messageManager';
 
 //function used to export a form 
-
 export const MessageForm = () => {
+
+    // declaration used to call for the authenticated logged in user.
+    const currentUser = parseInt(sessionStorage.getItem("nutshell_user"));
 
     // variables and props
     const [message, setMessage] = useState({
-        userId: parseInt(sessionStorage.getItem("nutshell_user")),
+        userId: currentUser,
         message: "",
         recipientId: 0,
         timestamp: new Date().toLocaleString()
     })
 
-    // More variables to use 
+    // More variables to use to help modify state
     const [users, setUsers] = useState([]);
 
     // used State to set variables for waiting to tell the DOM what to render
@@ -29,11 +31,33 @@ export const MessageForm = () => {
 
     // function used to control the change of form to get a new message 
     const handleControlledInputChange = (event) => {
+        // Creating a new message will modify the array so we need a copy
         const newMessage = { ...message }
+        // We will holding onto the value of a selection TBD
         let selectedVal = event.target.value
-        if (event.target.id.includes('Id')) {
-            selectedVal = parseInt(selectedVal)
+        // Now the fun begins. "Regular expressions are used to perform pattern-matching and "search-and-replace" functions on text." -W3
+        // We need to search for a friend with an @ and have an _. 
+        if (selectedVal.startsWith(`@`)) {
+        // We now will define a variable to hold onto a sequence of characters we will later use to match our search pattern. 
+        // This will represent a search starting after an @. The backslash are used ti escape the special characters.  
+        let expressionStatement = /(?<=\@)(.*?)(?=\s)/;
+        // A variable is now declared in order to help regular expression pattern match. 
+        // We are using the abpve targeted event to help search.
+        let searchedForName = selectedVal.match(expressionStatement)
+        // The code will only work if the searchedForName is not of null value.
+        if ( searchedForName !== null ) {
+            // We are now using the .replace of regular expression in order to help the code compile.
+            // The "g" after the expression for global search.
+            searchedForName = searchedForName[0].replace(/_/g, ' ')
+            //  Now we need to call for the users to be searched through in order to find our recipient.
+            users.forEach(user => {
+                // We are creating a conditionional to determine if the name matches the logged in user
+                if ( searchedForName === user.name && user.id !== currentUser ) {
+                    newMessage.recipientId = user.id;
+                }
+            })
         }
+    }
         newMessage[event.target.id] = selectedVal
         setMessage(newMessage)
     }
